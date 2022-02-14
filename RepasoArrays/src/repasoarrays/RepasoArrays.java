559c1dc16be1b3599b6607043d4a8d2ccda36c76
DavidHermi <91197896+DavidHermi@users.noreply.github.com>
2022-02-14 10:56:02 +0100

    Add files via upload

---
+++ RepasoArrays/src/repasoarrays/RepasoArrays.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repasoarrays;
+
+/**
+ *
+ * @author dam1
+ */
+public class RepasoArrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+       
+               int[] numeros = {1,2,3,4,5,6} ;
+        
+              
+Ver.visualizar(numeros);
+        
+
+        
+    }
+    
+}
---
+++ RepasoArrays/src/repasoarrays/Ver.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repasoarrays;
+
+/**
+ *
+ * @author dam1
+ */
+public class Ver {
+    
+    public static void visualizar (int[] numeros){
+     
+   
+	
+    
+      for (int i=0;i<numeros.length;i++ ) {
+      
+      
+          System.out.print(numeros[i]);
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      }
+      
+    }
+    
+    
+    
+    
+    
+}
